0d1ea50378e2b3dfe121780b86351e4a75be4b72
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-07 15:53:58 -0700

    initial structure and styling for SelfReflectionPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './styles/App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
+import SelfReflectionPage from './pages/SelfReflectionPage/SelfReflectionPage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/self-reflection" element={<SelfReflectionPage />} />
       </Routes>
     </BrowserRouter>
     </main>
--- src/pages/SelfReflectionPage/SelfReflectionPage.jsx
+++ src/pages/SelfReflectionPage/SelfReflectionPage.jsx
@@ -3,6 +3,18 @@ import './SelfReflectionPage.scss';
 function SelfReflectionPage() {
     return (
         <section className="self-reflection-page">
+            <div className="self-reflection-page__content">
+                <h1 className="self-reflection-page__title">Self-Reflection</h1>
+                <div className="chat">
+                    <div className="chat__display"></div>
+                    <div className="prompt">
+                        <textarea className="prompt__input" placeholder="Enter your prompt here..."/>
+                        <button className="prompt__submit-btn">Enter</button>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
+
+export default SelfReflectionPage;
