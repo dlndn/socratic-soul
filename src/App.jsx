0bf1272d4700ce44a8ea9d01dc91024dd63e5a2a
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-07 22:34:53 -0700

    formatted App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import './styles/App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import MainPage from './pages/MainPage/MainPage';
-import SelfReflectionPage from './pages/SelfReflectionPage/SelfReflectionPage';
-import SelfExpressionPage from './pages/SelfExpressionPage/SelfExpressionPage';
-import PageNotFound from './pages/PageNotFound/PageNotFound';
-
+import "./styles/App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import MainPage from "./pages/MainPage/MainPage";
+import SelfReflectionPage from "./pages/SelfReflectionPage/SelfReflectionPage";
+import SelfExpressionPage from "./pages/SelfExpressionPage/SelfExpressionPage";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
-  return (
-    <main className="app">
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/self-reflection" element={<SelfReflectionPage />} />
-        <Route path="/self-expression" element={<SelfExpressionPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-    </main>
-  )
+    return (
+        <main className="app">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route
+                        path="/self-reflection"
+                        element={<SelfReflectionPage />}
+                    />
+                    <Route
+                        path="/self-expression"
+                        element={<SelfExpressionPage />}
+                    />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </main>
+    );
 }
 
-export default App
+export default App;
