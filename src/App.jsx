4006fb0ead96d42e5a11e5093fb8710c49a88b40
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-07 22:09:44 -0700

    initial structure and styling of Header, adjusted partials

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './styles/App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import MainPage from './pages/MainPage/MainPage';
 import SelfReflectionPage from './pages/SelfReflectionPage/SelfReflectionPage';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
+
 
 function App() {
   return (
     <main className="app">
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/self-reflection" element={<SelfReflectionPage />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,40 @@
-import './Header.scss';
+import "./Header.scss";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
+    const [currentPage, setCurrentPage] = useState("home");
+
+    function changeCurrentPage(page) {
+        setCurrentPage(page);
+    }
+
     return (
         <header className="header">
+            <h1 className="header__logo">Socratic Soul</h1>
+            <nav className="navbar">
+                <Link 
+                    to={"/"} 
+                    className={`navbar__link navbar__link--home ${currentPage === "home" &&  "navbar__link--active"}`}
+                    onClick={() => changeCurrentPage("home")}
+                >
+                    <p className="navbar__text">Home</p>
+                </Link>
+                <Link 
+                    to={"/self-reflection"} 
+                    className={`navbar__link navbar__link--self-reflection ${currentPage === "self-reflection" &&  "navbar__link--active"}`}
+                    onClick={() => changeCurrentPage("self-reflection")}
+                >
+                    <p className="navbar__text">Self-Reflection</p>
+                </Link>
+                <Link 
+                    to={"/self-expression"} 
+                    className={`navbar__link navbar__link--self-expression ${currentPage === "self-expression" &&  "navbar__link--active"}`}
+                    onClick={() => changeCurrentPage("self-expression")}
+                >
+                    <p className="navbar__text">Self-Expression</p>
+                </Link>
+            </nav>
         </header>
     );
 }
