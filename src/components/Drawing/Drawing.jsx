21096b3b630e5690d02623e1155847b84dd1b86a
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-09 16:54:42 -0700

    renamed Sketch component to Drawing

---
+++ src/components/Drawing/Drawing.jsx
@@ -0,0 +1,9 @@
+import "./Drawing.scss";
+
+function Drawing() {
+    return (
+        <h1>Drawing Page</h1>
+    );
+}
+
+export default Drawing;
--- src/components/Sketch/Sketch.jsx
+++
@@ -1,9 +0,0 @@
-import "./Sketch.scss";
-
-function Sketch() {
-    return (
-        <h1>Sketch Page</h1>
-    );
-}
-
-export default Sketch;
