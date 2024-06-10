9f9dc3ccb49ad83f3aaae99389e3a5acb30072c0
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-09 20:43:02 -0700

    restructured and restyled MainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,29 +1,31 @@
-import './MainPage.scss';
-import { Link } from 'react-router-dom';
+import "./MainPage.scss";
+import { Link } from "react-router-dom";
 
 function MainPage() {
     return (
         <section className="main-page">
             <div className="main-page__content">
                 <h1 className="main-page__title">Socratic Soul</h1>
-                <div className="activities">
-                    <h2 className="activities__title">Activities</h2>
-                    <h3 className="activities__category">Self-Reflection</h3>
-                    <div className="activities__row">
-                        <Link to={'/self-reflection/emotions'} className="activity">
-                            <h4 className="activity__title">Emotions</h4>
-                            <p className="activity__description">Chat with an AI who will guide you to better understand your current feelings.</p>
+                <div className="activity-category">
+                    <h2 className="activity-category__title">Activity Categories</h2>
+                    <div className="activity-category__row">
+                        <Link
+                            to={"/self-reflection"}
+                            className="category"
+                        >
+                            <h4 className="category__title">Self-Reflection</h4>
+                            <p className="category__description">
+                                Chat with an AI to explore and understand better your emotions and/or relationships. 
+                            </p>
                         </Link>
-                        <Link to={'/self-reflection/relationships'} className="activity">
-                            <h4 className="activity__title">Relationships</h4>
-                            <p className="activity__description">Chat with an AI to reflect and evaluate on current relationship(s).</p>
-                        </Link>
-                    </div>
-                    <h3 className="activities__category">Self-Expression</h3>
-                    <div className="activities__row">
-                        <Link to={'/self-expression/drawing'} className="activity">
-                            <h4 className="activity__title">Drawing</h4>
-                            <p className="activity__description">Express how you feel through drawing and coloring!</p>
+                        <Link
+                            to={"/self-expression"}
+                            className="category"
+                        >
+                            <h4 className="category__title">Self-Expression</h4>
+                            <p className="category__description">
+                                Do activities that use creative outlets to express your thoughts and emotions.
+                            </p>
                         </Link>
                     </div>
                 </div>
