023b75a3f697e2cf22390d55a8f28cf0092780d4
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-09 22:06:04 -0700

    added image, quotes, and welcome message to MainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,16 @@
 import "./MainPage.scss";
 import { Link } from "react-router-dom";
+import greekBustImg from "../../assets/images/kintsugi-greek-bust.png";
 
 function MainPage() {
     return (
         <section className="main-page">
             <div className="main-page__content">
                 <h1 className="main-page__title">Socratic Soul</h1>
+                <img src={greekBustImg} alt="An Ancient Greek bust inlined with cracks of gold." className="main-page__img" />
+                <h4 className="main-page__quote">"True wisdom comes to each of us when we realize how little we understand about life, ourselves, and the world around us."</h4>
+                <p className="main-page__quote-author">- Socrates</p>
+                <p className="main-page__welcome-message">Welcome to Socratic Soul! Here is a place to learn more about yourself and to express who you are. To begin, please select an activity category below.</p>
                 <div className="activity-category">
                     <h2 className="activity-category__title">Activity Categories</h2>
                     <div className="activity-category__row">
