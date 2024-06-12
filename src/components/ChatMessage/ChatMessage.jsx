2c4fe76973f4a9243cad07dca482e558dbb26435
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-12 01:37:03 -0700

    additional styling for ChatMessages, Chatroom, and Drawing

--- src/components/ChatMessage/ChatMessage.jsx
+++ src/components/ChatMessage/ChatMessage.jsx
@@ -1,5 +1,6 @@
 import "./ChatMessage.scss";
 import femaleSilhouetteImg from "../../assets/images/female-head-silhouette.svg";
+import headSilhouetteImg from "../../assets/images/head-side-view.svg"
 import Markdown from 'react-markdown';
 
 function ChatMessage({ role, text }) {
@@ -7,7 +8,9 @@ function ChatMessage({ role, text }) {
         <div className="message">
             {role === "model" && ( // renders if message is from the AI
                 <div className="message__role-wrapper message__role-wrapper--model">
-                    <img className="message__user-img" src={femaleSilhouetteImg} alt="A profile silhouette of a woman's head." />
+                    <div className="message__role-background message__role-background--model">
+                        <img className="message__img message__img--model" src={femaleSilhouetteImg} alt="A profile silhouette of a woman's head." />
+                    </div>
                 </div>
             )}
             <div className="message__text-wrapper">
@@ -15,7 +18,9 @@ function ChatMessage({ role, text }) {
             </div>
             {role === "user" && ( // renders if message is from user
                 <div className="message__role-wrapper message__role-wrapper--user">
-                    <p className="message__role">You</p>
+                    <div className="message__role-background message__role-background--user">
+                        <img className="message__img message__img--user" src={headSilhouetteImg} alt="A profile silhoutte of someone's head." />
+                    </div>
                 </div>
             )}
         </div>
