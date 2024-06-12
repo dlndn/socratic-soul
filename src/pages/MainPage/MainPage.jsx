a4a64313850bf42cba3d6937bf0955d0b2a3cb61
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-12 02:11:24 -0700

    changed MainPage image; further styling for MainPage, Chatroom, and ChatMessage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,6 @@
 import "./MainPage.scss";
 import { Link } from "react-router-dom";
-import greekBustImg from "../../assets/images/kintsugi-greek-bust.png";
-import brokenGreekBustImg from "../../assets/images/broken-bust-male-no-bg.png";
+import wingedVictoryImg from "../../assets/images/winged-victory-1.jpg";
 
 function MainPage() {
     return (
@@ -9,7 +8,8 @@ function MainPage() {
             <div className="main-page__content">
                 <h1 className="main-page__title">Socratic Soul</h1>
                 <p className="main-page__slogan">Ask more. Think more. Express more.</p>
-                <img src={brokenGreekBustImg} alt="A broken Ancient-Greek-style bust." className="main-page__img" />
+                <img src={wingedVictoryImg} alt="The Niké of Samothrace statue." className="main-page__img" />
+                <p className="main-page__img-label">"Niké of Samothrace" in Louvre Museum</p>
                 <h4 className="main-page__quote">"True wisdom comes to each of us when we realize how little we understand about life, ourselves, and the world around us."</h4>
                 <p className="main-page__quote-author">- Socrates</p>
                 <p className="main-page__welcome-message">Welcome to Socratic Soul! Here is a place to learn more about yourself and to express who you are. To begin, please select an activity category below.</p>
